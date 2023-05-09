384352ee1afbfcb709acb59bc70a1c5295e52ec5
tmax818 <tylermaxwell661@gmail.com>
2023-05-09 12:37:05 -0600

    update webstorm

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+const users = [
+  { firstName: "Reimu",  lastName: "Hakurei"    },
+  { firstName: "Marisa", lastName: "Kirisame"   },
+  { firstName: "Sanae",  lastName: "Kochiya"    },
+  { firstName: "Sakuya", lastName: "Izayoi"     },
+  { firstName: "Momiji", lastName: "Inubashiri" }
+];
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/api', function(req, res, next) {
+  res.json(users);
 });
 
 module.exports = router;
